15096e5bf63b86d25ed2fad5f93d1429b183f954
Kkuntal990 <kokatekuntal@gmail.com>
2021-04-12 23:50:45 +0530

    added load_dataset code

--- VIBNet/test.py
+++ VIBNet/test.py
@@ -8,52 +8,7 @@ import numpy as np
 import argparse
 import tensorflow as tf
 from tensorflow_probability import distributions as ds
-
-
-def to_onehot(yy):
-        yy1 = np.zeros([len(yy), max(yy) + 1])
-        yy1[np.arange(len(yy)), yy] = 1
-        return yy1
-
-def load_data(PATH):
-    with open(PATH, 'rb') as xd1:  
-        Xd = pickle.load(xd1, encoding='latin1')  # , encoding='latin1'
-        snrs, mods = map(lambda j: sorted(
-            list(set(map(lambda x: x[j], Xd.keys())))), [1, 0])
-    X = []
-    lbl = []
-    SNR = []
-    for mod in mods:
-        for snr in snrs:
-            SNR.append(snr)
-            X.append(Xd[(mod, snr)])
-            for i in range(Xd[(mod, snr)].shape[0]):
-                lbl.append((mod, snr))
-    X = np.vstack(X)
-    np.random.seed(2016)  
-    n_examples = X.shape[0]
-    n_train = n_examples * 0.5  # ĺŻšĺ
-    train_idx = np.random.choice(
-        range(0, n_examples), size=int(n_train), replace=False)
-    test_idx = list(set(range(0, n_examples)) - set(train_idx))  # label
-    test_idx2 = []
-    X_train = X[train_idx]
-    for i in test_idx:
-        if args.SNR:
-
-            if SNR[i//1000] == args.SNR:
-                test_idx2.append(i)
-        else:
-            if SNR[i//1000] >= 0:
-                test_idx2.append(i)
-
-    
-    X_test = X[test_idx2]
-    
-    trainy = list(map(lambda x: mods.index(lbl[x][0]), train_idx))
-    Y_train = to_onehot(trainy)
-    Y_test = to_onehot(list(map(lambda x: mods.index(lbl[x][0]), test_idx2)))
-    return X_train, X_test, Y_train, Y_test,mods
+from load_dataset import load_data
 
 
 parser = argparse.ArgumentParser()
@@ -64,7 +19,7 @@ args = parser.parse_args()
 SNR_filter = args.SNR
 PATH = args.Path
 
-X_train, X_test, Y_train, Y_test,mods = load_data(PATH)
+X_train, X_test, Y_train, Y_test,mod_train, mod_test, mods = load_data(PATH)
 in_shp = list(X_train.shape[1:])
 print(X_train.shape, X_test.shape)
 print(Y_train.shape, Y_test.shape)
