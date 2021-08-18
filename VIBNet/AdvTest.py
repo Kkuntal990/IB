ecfcd2e52529bd3d4ad60e16f69c9c10a2f15d2e
Kkuntal990 <kokatekuntal@gmail.com>
2021-08-19 02:13:11 +0530

    advtest changed

--- VIBNet/AdvTest.py
+++ VIBNet/AdvTest.py
@@ -20,57 +20,57 @@ def AdversarialCompare(PATH, model1, model2, SNR_Filter=list(range(19)), max_eps
 
     #printing acc of VIB
     Y_pred = np.argmax(VIB(X_test), axis=1)
-     Y_test2 = np.argmax(Y_test, axis=1)
-      co = 0
-       for i in range(len(X_test)):
-            if(Y_test2[i] == Y_pred[i]):
-                co += 1
-        print(co/len(X_test))
+    Y_test2 = np.argmax(Y_test, axis=1)
+    co = 0
+    for i in range(len(X_test)):
+        if(Y_test2[i] == Y_pred[i]):
+            co += 1
+    print(co/len(X_test))
 
-        #getting accuracy of CNN
-        Y_pred = np.argmax(CNN(X_test), axis=1)
-        Y_test2 = np.argmax(Y_test, axis=1)
-        co = 0
-        for i in range(len(X_test)):
-            if(Y_test2[i] == Y_pred[i]):
-                co += 1
-        print(co/len(X_test))
+    #getting accuracy of CNN
+    Y_pred = np.argmax(CNN(X_test), axis=1)
+    Y_test2 = np.argmax(Y_test, axis=1)
+    co = 0
+    for i in range(len(X_test)):
+        if(Y_test2[i] == Y_pred[i]):
+            co += 1
+    print(co/len(X_test))
 
-        classes = mods
-        print(in_shp)
-        eps = [1e-4, 5*1e-4, 1e-3, 5*1e-3, 8*1e-3,
-               1e-2, 2*1e-2, 5*1e-2, 8*1e-2, 1e-1]
-        OP = []
+    classes = mods
+    print(in_shp)
+    eps = [1e-4, 5*1e-4, 1e-3, 5*1e-3, 8*1e-3,
+            1e-2, 2*1e-2, 0.03, 0.04, 5*1e-2, 0.65, 8*1e-2, 1e-1, 0.5]
+    OP = []
 
-        for __ in eps:
-            tmp = []
-            for i in range(times):
-                X_Adv = projected_gradient_descent(
-                    VIB, X_test, __, __/ratio, 40, np.inf)
-                Y_pred = np.argmax(VIB(X_Adv), axis=1)
-                Y_test2 = np.argmax(Y_test, axis=1)
-                co = 0
-                for i in range(len(X_test)):
-                    if(Y_test2[i] == Y_pred[i]):
-                        co += 1
-                print(co/len(X_test))
-                tmp.append(co/len(X_test))
-            OP.append(np.min(tmp))
+    for __ in eps:
+        tmp = []
+        for i in range(times):
+            X_Adv = projected_gradient_descent(
+                VIB, X_test, __, __/ratio, 40, np.inf)
+            Y_pred = np.argmax(VIB(X_Adv), axis=1)
+            Y_test2 = np.argmax(Y_test, axis=1)
+            co = 0
+            for i in range(len(X_test)):
+                if(Y_test2[i] == Y_pred[i]):
+                    co += 1
+            print(co/len(X_test))
+            tmp.append(co/len(X_test))
+        OP.append(np.min(tmp))
 
-        OP2 = []
-        for __ in eps:
-            tmp = []
-            for i in range(times):
-                X_Adv = projected_gradient_descent(
-                    CNN, X_test, __, __/ratio, 40, np.inf)
-                Y_pred = np.argmax(CNN(X_Adv), axis=1)
-                Y_test2 = np.argmax(Y_test, axis=1)
-                co = 0
-                for i in range(len(X_test)):
-                    if(Y_test2[i] == Y_pred[i]):
-                        co += 1
-                print(co/len(X_test))
-                tmp.append(co/len(X_test))
-            OP2.append(np.min(tmp))
+    OP2 = []
+    for __ in eps:
+        tmp = []
+        for i in range(times):
+            X_Adv = projected_gradient_descent(
+                CNN, X_test, __, __/ratio, 40, np.inf)
+            Y_pred = np.argmax(CNN(X_Adv), axis=1)
+            Y_test2 = np.argmax(Y_test, axis=1)
+            co = 0
+            for i in range(len(X_test)):
+                if(Y_test2[i] == Y_pred[i]):
+                    co += 1
+            print(co/len(X_test))
+            tmp.append(co/len(X_test))
+        OP2.append(np.min(tmp))
 
-        return eps, OP, OP2
+    return eps, OP, OP2
